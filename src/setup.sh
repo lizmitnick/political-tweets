1d0f71c2a52461983eb3544d1eb74161a548aa19
Liz Mitnick <liz@acornanalytics.io>
2020-02-23 10:18:39 -0800

    updates for venv first

--- src/setup.sh
+++ src/setup.sh
@@ -2,17 +2,18 @@
 
 # From wherever you want to install the directory and run the program:
 
+
+virtualenv --python=/usr/local/bin/python3 venv
+source venv/bin/activate
 git clone https://github.com/jonbakerfish/TweetScraper.git
 cd TweetScraper
 touch .env
-virtualenv --python=/usr/local/bin/python3 venv
-source venv/bin/activate
 pip install -r requirements.txt
-pip install pandas numpy datetime python-dotenv simplejson
+pip install pandas numpy datetime python-dotenv simplejson scrapy pymongo mysql-connector configparser
 pip freeze > requirements.txt
 mkdir src
 mkdir inputs
 cd ..
-cp tweet_master.py ./TweetScraper/src
-cp issues_terms.json ./TweetScraper/inputs
+cp src/candidate_tweets.py ./TweetScraper/src
+cp inputs/issues_terms.json ./TweetScraper/inputs
 cd Tweetscraper
